188265b0a6b35c58daaf5a88aad712b0db9b6e75
Yann Malet <yann.malet@gmail.com>
2014-02-15 15:05:01 +0100

    Adding eventsource into the mix

--- main.go
+++ main.go
@@ -1,11 +1,66 @@
 package main
 
-import "github.com/codegangsta/martini"
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/codegangsta/martini"
+	"github.com/donovanhide/eventsource"
+)
+
+type Comment struct {
+	id           string
+	Author, Text string
+}
+
+func (c *Comment) Id() string    { return c.id }
+func (c *Comment) Event() string { return "comment" }
+func (c *Comment) Data() string {
+	b, err := json.Marshal(c)
+	if err != nil {
+		log.Fatal("An error occured while trying to marshal the comment", err)
+	}
+	log.Println("DEBUG: data", string(b))
+	return string(b)
+}
+
+var comments = []Comment{
+	{"11", "yml", "Governments struggle to control global price of gas"},
+	{"12", "marco", "Tomorrow is another day"},
+	{"13", "martin", "News for news' sake"},
+}
+
+func newRepo(srv *eventsource.Server) *eventsource.SliceRepository {
+	repo := eventsource.NewSliceRepository()
+	srv.Register("comments", repo)
+	for i := range comments {
+		log.Println("DEBUG: comment index", i)
+		repo.Add("comments", &comments[i])
+	}
+	return repo
+}
+
+func replayRepo(srv *eventsource.Server, repo *eventsource.SliceRepository) {
+	for {
+		// Hard coding the replay from 12
+		for event := range repo.Replay("comments", "12") {
+			srv.Publish([]string{"comments"}, event)
+		}
+		<-time.After(time.Second * 5)
+	}
+}
 
 func main() {
+	srv := eventsource.NewServer()
+	defer srv.Close()
+	repo := newRepo(srv)
+	go replayRepo(srv, repo)
+
 	m := martini.Classic()
 	m.Get("/hello", func() string {
 		return "Hello world!"
 	})
+	m.Any("/eventsource", srv.Handler("comments"))
 	m.Run()
 }
--- public/js/react-comment.js
+++ public/js/react-comment.js
@@ -1,85 +1,103 @@
-var converter = new Showdown.converter();
-
-var CommentBox = React.createClass({
-    loadComments: function() {
-		var instance = this;
-        $.ajax({
-            url: this.props.url,
-            dataType: "json",
-            success: function(data) {
-                instance.setState({data: data});
-            }
-       });
-    },
-	getInitialState: function(){
-		return {data: []};
-	},
-    componentWillMount: function() {
-        this.loadComments();
-        setInterval(this.loadComments, this.props.pollInterval);
-    },
-	render: function () {
-		return (
-			React.DOM.div(
-				{
-					className: "CommentBox",
-				},
-				"Hello, world! I am a ",
-				React.DOM.strong(null, "commentbox"),
-				CommentList({data: this.state.data}),
-				CommentForm({})));
-	}
-});
-
-var CommentList = React.createClass({
-	render: function() {
-		var commentNodes = this.props.data.map(function (item, index) {
-			return Comment({key: index, author: item.author, children: item.text});
-		});
-		return (
-			React.DOM.div({
-					className: "commentList",
-				},
-				"Hello, world! I am a ",
-				React.DOM.strong(null, "CommentList"),
-				commentNodes
-				));
-	}
-});
-
-var Comment = React.createClass({
-	render : function() {
-		var htmlComment = converter.makeHtml(this.props.children.toString());
-		return (
-			React.DOM.div({
-					className: "comment",
-				},
-				React.DOM.h2({
-						className: "commentAuthor",
+(function (){
+	use = "strict";
+	var source = new EventSource("/eventsource/");
+	var converter = new Showdown.converter();
+	
+	var CommentBox = React.createClass({
+		loadComments: function() {
+			$.ajax({
+				url: this.props.url,
+				dataType: "json",
+				success: function(data) {
+					this.setState({data: data});
+				}.bind(this)
+		   })
+		},
+		
+		updateComments: function(comment) {
+			if (this.state.data.map(function(item) {return item.Id}).indexOf(comment.Id) === -1){
+				this.state.data.push(comment);
+				this.setState({data: this.state.data});
+			};
+		},
+		
+		getInitialState: function(){
+			return {data: []};
+		},
+		
+		componentWillMount: function() {
+			this.loadComments();
+			source.addEventListener('comment', function(e) {
+				var cmt = JSON.parse(e.data);
+				cmt.Id = e.lastEventId;
+				this.updateComments(cmt);
+			}.bind(this), false);
+			
+		},
+		
+		render: function () {
+			return (
+				React.DOM.div(
+					{
+						className: "CommentBox",
 					},
-					this.props.author),
-					React.DOM.span({
-							dangerouslySetInnerHTML:{__html:htmlComment}
-						})));
-	}
-});
-
-var CommentForm = React.createClass({
-	render: function() {
-		return (
-			React.DOM.div({
-					className: "commentBox",
-				},
-				"Hello, world! I am a ",
-				React.DOM.strong(null, "CommentForm")
-				));
-	}
-});
-
-React.renderComponent(
-    CommentBox({
-        url: "/data/comments.json",
-        pollInterval: 5000
-    }),
-    document.getElementById('content')
-);
+					"Hello, world! I am a ",
+					React.DOM.strong(null, "commentbox"),
+					CommentList({data: this.state.data}),
+					CommentForm({})));
+		}
+	});
+	
+	var CommentList = React.createClass({
+		render: function() {
+			var commentNodes = this.props.data.map(function (item, index) {
+				return Comment({key: index, author: item.Author, children: item.Text});
+			});
+			return (
+				React.DOM.div({
+						className: "commentList",
+					},
+					"Hello, world! I am a ",
+					React.DOM.strong(null, "CommentList"),
+					commentNodes
+					));
+		}
+	});
+	
+	var Comment = React.createClass({
+		render : function() {
+			var htmlComment = converter.makeHtml(this.props.children.toString());
+			return (
+				React.DOM.div({
+						className: "comment",
+					},
+					React.DOM.h2({
+							className: "commentAuthor",
+						},
+						this.props.author),
+						React.DOM.span({
+								dangerouslySetInnerHTML:{__html:htmlComment}
+							})));
+		}
+	});
+	
+	var CommentForm = React.createClass({
+		render: function() {
+			return (
+				React.DOM.div({
+						className: "commentBox",
+					},
+					"Hello, world! I am a ",
+					React.DOM.strong(null, "CommentForm")
+					));
+		}
+	});
+	
+	React.renderComponent(
+		CommentBox({
+			url: "/data/comments.json",
+			pollInterval: 5000
+		}),
+		document.getElementById('content')
+	);
+})();
